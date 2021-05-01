a206aef7606b5bb635612513298d3dfde6e56995
zarriei <75182441+zarriei@users.noreply.github.com>
2021-05-01 00:31:46 -0400

    final application

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./App.css";
 import Header from './components/Header';
-import EmployeesContainer from './components/container/EmployeesContainer';
+import EmployeesContainer from '../src/components/Container/EmployeesContainer';
 
 function App() {
   return (
